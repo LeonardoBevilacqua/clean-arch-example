f90496656f4e2eb41460bd6763525540a878e71f
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2022-11-25 14:50:17 -0300

    creating test for list all todos use case

---
+++ src/todos/use-case/list-all-todos/List-all-todos.use-case.spec.ts
@@ -0,0 +1,22 @@
+import { Todo, TodoProps } from "../../domain/Todo.entity";
+import { TodoInMemoryRepository } from "../../infra/in-memory/Todo-in-memory.repository";
+import { ListAllTodosUseCase } from "./List-all-todos.use-case";
+
+describe('ListAllTodosUseCase Tests', () => {
+
+    it('should list all todos', async () => {
+        const repository = new TodoInMemoryRepository();
+        const todoProps: TodoProps = {
+            text: "test",
+            day: "Today",
+            reminder: false
+        };
+        const todo = Todo.create(todoProps);
+        repository.items = [todo];
+
+        const listAllUseCase = new ListAllTodosUseCase(repository);
+        const output = await listAllUseCase.execute();
+        expect(output).toHaveLength(1);
+        expect(output).toStrictEqual([todo.toJSON()]);
+    });
+})
