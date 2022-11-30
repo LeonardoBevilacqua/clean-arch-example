3f1c178a520278ba7d97c5b2010edace1dbede48
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2022-11-30 15:32:28 -0300

    updating unit tests

--- src/todos/infra/db/sequelize/Todo-sequelize.repository.spec.ts
+++ src/todos/infra/db/sequelize/Todo-sequelize.repository.spec.ts
@@ -1,9 +1,13 @@
+import { Sequelize } from "sequelize";
 import { Todo, TodoProps } from "../../../domain/Todo.entity";
-import { TodoInMemoryRepository } from "./Todo-in-memory.repository"
+import { TodoSequelizeRepository } from "./Todo-sequelize.repository";
 
-describe("TodoInMemoryRepository Test", () => {
-    const repository = TodoInMemoryRepository.Instance;
+describe("TodoSequelizeRepository Test", () => {
+    const sequelize = new Sequelize('sqlite::memory:');
+    const repository = new TodoSequelizeRepository(sequelize);
     let todo: Todo;
+
+    beforeEach(async () => await sequelize.sync());
 
     test("should insert a new todo", async () => {
         const todoProps: TodoProps = {
@@ -12,20 +16,26 @@ describe("TodoInMemoryRepository Test", () => {
             reminder: false
         };
         todo = Todo.create(todoProps, 1);
-        await repository.insert(todo);
-        expect(repository.items).toHaveLength(1);
-        expect(repository.items).toStrictEqual([todo]);
+        const result = await repository.insert(todo);
+        expect(result.text).toStrictEqual(todo.text);
+        expect(result.day).toStrictEqual(todo.day);
+        expect(result.reminder).toStrictEqual(todo.reminder);
     })
 
     test("should return all todos", async () => {
         const output = await repository.findAll();
         expect(output).toHaveLength(1);
-        expect(output).toStrictEqual([todo]);
+        expect(output[0].text).toStrictEqual(todo.text);
+        expect(output[0].day).toStrictEqual(todo.day);
+        expect(output[0].reminder).toStrictEqual(todo.reminder);
     })
 
     test("should return a todo by id", async () => {
         const output = await repository.findById(Number(todo.id));
-        expect(output).toStrictEqual(todo);
+        expect(output).toBeDefined();
+        expect(output?.text).toStrictEqual(todo.text);
+        expect(output?.day).toStrictEqual(todo.day);
+        expect(output?.reminder).toStrictEqual(todo.reminder);
     })
 
     test("should return undefined", async () => {
@@ -40,19 +50,12 @@ describe("TodoInMemoryRepository Test", () => {
             reminder: false
         };
         const updatedTodo = Todo.create(todoProps, 2);
-        repository.items.push(updatedTodo);
 
         updatedTodo.updateText("updated test")
         await repository.update(updatedTodo);
-        expect(repository.items).toHaveLength(2);
-        expect(repository.items).toStrictEqual([todo, updatedTodo]);
-        expect(repository.items[1].text).toStrictEqual("updated test");
     })
 
     test("should remove a todo by id", async () => {
-        repository.items = [todo];
         await repository.delete(Number(todo.id));
-        expect(repository.items).toHaveLength(0);
-        expect(repository.items).toStrictEqual([]);
     })
 })
--- src/todos/use-case/create-todo/Create-todo.use-case.spec.ts
+++ src/todos/use-case/create-todo/Create-todo.use-case.spec.ts
@@ -1,9 +1,15 @@
-import { TodoInMemoryRepository } from "../../infra/db/in-memory/Todo-in-memory.repository"
+import { Sequelize } from "sequelize";
+import { TodoSequelizeRepository } from "../../infra/db";
+import { TodoInMemoryRepository } from "../../infra/db/in-memory/Todo-in-memory.repository";
 import { CreateTodoUseCase } from "./Create-todo.use-case";
 
 describe('CreateTodoUseCase Tests', () => {
+    const sequelize = new Sequelize('sqlite::memory:');
+    const todoSequelizeRepository = new TodoSequelizeRepository(sequelize);
 
-    it('should create a new todo', async () => {
+    beforeAll(async () => await sequelize.sync());
+
+    it('should create a new todo for in memory repository', async () => {
         const repository = TodoInMemoryRepository.Instance;
         const createUseCase = new CreateTodoUseCase(repository);
         const output = await createUseCase.execute({
@@ -19,5 +25,18 @@ describe('CreateTodoUseCase Tests', () => {
             day: "Today",
             reminder: false
         });
+    })
+
+    it('should create a new todo for in sequelize repository', async () => {
+        const createUseCase = new CreateTodoUseCase(todoSequelizeRepository);
+        const output = await createUseCase.execute({
+            text: "test",
+            day: "Today",
+            reminder: false
+        });
+
+        expect(output.text).toStrictEqual("test");
+        expect(output.day).toStrictEqual("Today");
+        expect(output.reminder).toStrictEqual(false);
     })
 })
